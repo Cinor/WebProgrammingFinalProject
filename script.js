dcbd30dd92c5f250e3e004acfb6d35098e929326
FinianWu <apple@appletekiMacBook-Air-2.local>
2016-06-14 11:47:11 +0800

    update button

--- script.js
+++ script.js
@@ -6,10 +6,28 @@
   // map
   map = mapster.create(element, options);
 
+  var marker = map.addMarker({
+    lat: 25.150024,
+    lng: 121.7768812,
+    content: 'ntou'
+  });
+
   var marker2 = map.addMarker({
     lat: 25.1325611,
     lng: 121.739341,
     content: 'Keelung station'
+  });
+
+  var marker3 = map.addMarker({
+    lat: 25.1596123,
+    lng: 121.765433,
+    content: 'peaceful island'
+  });
+
+  var marker3 = map.addMarker({
+    lat: 25.1283,
+    lng: 121.7432,
+    content: 'front of tample'
   });
 
   //map._removeMarker(marker2);
