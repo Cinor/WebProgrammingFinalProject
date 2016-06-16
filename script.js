5c5095c1e3aa3560b89a663ca49bd1ec806d1785
FinianWu <apple@appletekiMacBook-Air-2.local>
2016-06-16 15:46:37 +0800

    update

--- script.js
+++ script.js
@@ -11,7 +11,32 @@ var marker1;
 
     marker = map.addMarker({
   });
+/*
+  var marker2 = map.addMarker({
+    lat: lat[1],
+    lng: lng[1],
+    content: 'Keelung station'
+  });
 
+  var marker3 = map.addMarker({
+    lat: lat[2],
+    lng: lng[2],
+    content: 'peaceful island'
+  });
+
+<<<<<<< HEAD
+  var marker4 = map.addMarker({
+    lat: lat[3],
+    lng: lng[3],
+    content: 'front of tample'
+=======
+  var marker3 = map.addMarker({
+    lat: 25.1283,
+    lng: 121.7432,
+    content: 'front of temple'
+>>>>>>> 9b391edbe7e6a4e92d37be32c65def80bb3766cb
+  });
+*/
   map._removeMarker(marker1);
 
   //console.log(map.markers);
